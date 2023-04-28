4604b7737132fa16ac84659984755a7bfec07236
pashadubikovskiy <pashadubikovskiy@gmail.com>
2023-04-28 08:36:27 +0300

    some filters

--- js/app.js
+++ js/app.js
@@ -1,32 +1,82 @@
-const { createApp } = Vue
-
-createApp({
+new Vue({
+    el: '#app',
     data() {
         return {
             message: 'description',
             info: {
-                username: null,
-                phone: null
+                username: 'Samantha',
+                phone: '1-463-123-4447'
             },
+            isLogged: false,
+            userInfo: null,
+            todoList: null,
+            usersId: null,
+            filterRules: {
+                status: 'all',
+                userId: 0,
+                filterTitle: '',
+            },
+            filteredTodoList: this.todoList,
         }
     },
     methods: {
-        checkFields: function() {
+        getTodo: function () {
+            const vm = this;
+            fetch('https://jsonplaceholder.typicode.com/todos', {cache: "no-cache"})
+                .then((response) => response.json())
+                .then((todos) => {
+                    vm.todoList = todos;
+                    vm.filteredTodoList = vm.todoList;
+                    vm.usersId = [...new Set(todos.map(point => point.userId))]
+                });
+        },
+        checkFields: function(e) {
+            e.preventDefault();
             const vm = this;
             fetch('https://jsonplaceholder.typicode.com/users', {cache: "no-cache"})
                 .then((response) => response.json())
                 .then((users) => {
                     let obj;
                     if (obj = users.find(user => user.username === vm.info.username)) {
+                        vm.userInfo = obj;
                         if (obj.phone === vm.info.phone) {
-                            vm.message = 'success';
+                            vm.isLogged = true;
+                            vm.getTodo();
                             return;
                         }
-                        vm.message = 'error'
+                        alert('error');
                         return;
                     }
-                    vm.message = 'error'
+                    alert('error');
                 });
+        },
+        filterAll: function () {
+            this.filteredTodoList = this.filteredTodoList.filter((item) => {
+                item.some(cat => ((cat.completed === this.status) && cat.title.includes(this.filterTitle) && cat.userId === this.userId))
+            })
+        },
+        filterByStatus: function () {
+            this.filteredTodoList = this.filteredTodoList.filter((item) => {
+                if (this.filterStatus === 'all') return item;
+                if (this.filterStatus === 'completed') {
+                   return item.completed === true;
+                }
+                if (this.filterStatus === 'uncompleted') {
+                   return item.completed === false;
+                }
+            })
+        },
+        filterByUser: function () {
+            this.filteredTodoList = this.filteredTodoList.filter((item) => {
+                if (!this.filterUserId) return item;
+                return item.userId === this.filterUserId;
+            })
+        },
+        filterByTitle: function () {
+            this.filteredTodoList = this.filteredTodoList.filter((item) => {
+                if (!this.filterTitle) return item;
+                return item.title.includes(this.filterTitle);
+            })
         }
     }
-}).mount('#app')
+})
